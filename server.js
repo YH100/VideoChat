05529c455979cd0b98d4776bfc08b1939a853751
YH100 <yuvalhendler100@gmail.com>
2019-01-29 22:15:06 +0200

    Adding express, socket.io

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var browserify = require('browserify-middleware');
 var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/xwww-
+app.use(session({secret: 'XASDASDA'}));
+
+var ssn;
+var usersSockets = [];
+
+var users = [
+    {userName: 'yossi', password: '123456'},
+    {userName: 'test', password: '123456'}];
 
 // -------------------- Socket.io --------------------
 // var socketEvents = require('./socketEvents');
@@ -8,20 +23,37 @@ var app = express();
 // socketEvents(io);
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-io.on("connection", () => {
-    console.log("a user is connected");
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    usersSockets.push(socket);
+
+
+    io.emit('newUser', {message: 'Hello everyone, a new user is connected'});
+    // for (var i = 0; i < usersSockets.length; i++)
+    // {
+    //
+    // }
+
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
-
-
-
 // -------------------- Socket.io --------------------
 
-// This responds with "Hello World" on the homepage
+
+// -------------------- Routes --------------------
 app.get('/', function (req, res) {
-    app.use(express.static(__dirname + '/' + 'public'));
+    // app.use(express.static(__dirname + '/' + 'public'));
     console.log("");
     console.log("-------------------- Got a GET request for the homepage -------------------");
     res.sendFile(__dirname + "/" + "index.html");
+});
+
+app.get('/login', function (req, res) {
+    console.log("-------------------- Got a GET request for login  -------------------");
+    res.sendFile(__dirname + "/public/html/login.html");
 });
 
 app.get('/example', (req, res) => {
@@ -30,10 +62,38 @@ app.get('/example', (req, res) => {
 });
 
 app.get('/index.js', browserify('./public/index.js'));
+// -------------------- Routes --------------------
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// -------------------- Post Form Requests --------------------
+app.post('/loginForm', function (req, res) {
+    console.log('loginForm: ' + req.body);
+    var userName = req.body.userName;
+    var password = req.body.password;
+    console.log(`got userName: ${userName}, password: ${password}`);
+
+    var isUserExist = users.some(function (currentUser) {
+        return currentUser.userName === userName &&
+            currentUser.password === password;
+    });
+
+    if (isUserExist) {
+        ssn = req.session;
+        ssn.userName = userName;
+        res.redirect("/");
+        // res.send("Approve!");
+    } else {
+        res.send("Not Approve!");
+    }
+});
+// -------------------- Post Form Requests --------------------
+
+
+//+++++++++++++++++++++++++++++++++++server run+++++++++++++++++++++++++++++++++++++++++++++++++
+
 var server = http.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
 });
+//+++++++++++++++++++++++++++++++++++server run+++++++++++++++++++++++++++++++++++++++++++++++++
