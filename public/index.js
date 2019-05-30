5b9e7247ea733b84281b75ee8cab9f6023dff2b2
YH100 <yuvalhendler100@gmail.com>
2019-05-30 19:57:49 +0300

    Adding comments

--- public/index.js
+++ public/index.js
@@ -1,16 +1,16 @@
 window.initiateVideoChat = function () {
-    var getUserMedia = require('getusermedia');
 
+    var getUserMedia = require('getusermedia');     //Loads the library code into the computer's immediate memory (ram)
     getUserMedia({video: true, audio: true}, function (err, stream) {
         if (err)
-            return console.error(err);
+            return console.error(err);              //If there is an error, it will return it
 
-        var startTime = window.performance.now();
-        var Peer = require('simple-peer');
+        var startTime = window.performance.now();  //Checks how long it took to produce the code
+        var Peer = require('simple-peer');         //Loads the library code into the computer's immediate memory (ram)
         var peer = new Peer({
-            initiator: location.hash === '#init',
-            trickle: false,
-            stream: stream
+            initiator: location.hash === '#init',  //Checks whether this client is initiating a call
+            trickle: false,                        //Trickle (ICE) is an optimization of the ICE specification for NAT traversal
+            stream: stream                         //Get the video ready to be send
         });
 
         peer.on('signal', function (data) {
@@ -51,38 +51,34 @@ window.initiateVideoChat = function () {
         });
 
         peer.on('stream', function (stream) {
-            $('#callWithLbl')[0].innerHTML = 'In a call with: ' + window.targetUser;
-            $('#closeMyVideoBtn')[0].style.visibility = 'visible';
+            $('#callWithLbl')[0].innerHTML = 'In a call with: ' + window.targetUser;    //Set a label with the name of the ather client
+            $('#closeMyVideoBtn')[0].style.visibility = 'visible';                      //Set my video btn to be visible
             $('#callButtons')[0].style.visibility = 'visible';
-            $('#loading')[0].style.visibility = 'hidden';
-            // var video = document.createElement('video')
-            // document.body.appendChild(video)
-            var video = document.getElementById('videoMain');
-            // video.src = window.URL.createObjectURL(stream)
+            $('#loading')[0].style.visibility = 'hidden';                               //Set the loding to be hidden
+            var video = document.getElementById('videoMain');                //Creat the element for the main video stream
             video.srcObject = stream;
-            video.play();
+            video.play();                                                              //Strat the striming
             var elem = document.getElementById("videoMain");
-
 
             // Create also my video
             var myVideo = document.getElementById('myVideo');
             myVideo.setAttribute('playsinline', '');
             myVideo.setAttribute('autoplay', '');
             myVideo.setAttribute('muted', '');
-            myVideo.style.width = '200px';
+            myVideo.style.width = '200px';                                            //Set the size of the video
             myVideo.style.height = '200px';
 
             /* Setting up the constraint */
-            var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+            var facingMode = "user";                                                  // Can be 'user' or 'environment' to access back or front camera (NEAT!)
             var constraints = {
-                audio: false,
+                audio: false,                                                         //dont play your mic audio
                 video: {
                     facingMode: facingMode
                 }
             };
 
             /* Stream it to video element */
-            navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+            navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {     //Get my feed
                 console.log('Start show my camera');
                 myVideo.srcObject = stream;
             });
